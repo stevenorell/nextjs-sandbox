3d17193c8b6975463a34df2bb1ee50fa6d50529c
Steve Norell <snorell@drakecooper.com>
2021-03-23 15:58:47 -0600

    Installed GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MN4M54D' })
+  }, []);
+
   return <Component {...pageProps} />
 }
 
