39375971c7e5c5d8e7c64e3a5535dba63b5eafb4
Steve Norell <snorell@drakecooper.com>
2021-03-23 14:41:39 -0600

    Set up basic demo and example sub-pages

--- pages/subpage-two.js
+++ pages/subpage-two.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App | Subpage One</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Demo Static Microsite
+          Subpage Two
         </h1>
 
         <p>Aenean nec aliquam nunc, sit amet euismod nisi. Morbi ac libero vitae purus suscipit imperdiet eu ut massa. Maecenas risus ligula, sagittis eget tempus placerat, facilisis vitae metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean metus nunc, semper non mi in, varius consequat leo. Aliquam at ex a tortor malesuada facilisis finibus vitae nibh. Donec justo nulla, venenatis vel mauris at, vestibulum efficitur ipsum. Mauris et ultrices odio, vitae posuere sapien. Cras sodales molestie aliquet. Suspendisse ut lectus ut nisi varius tincidunt.</p>
